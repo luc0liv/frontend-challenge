b660e339b51c1fa244e76762207baeeda7cfae8c
luc0liv <luoliveiradacunha@gmail.com>
2024-02-03 14:40:11 -0300

    feat: create Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { InputProps, InputTypes } from "../../types/input";
+import { SearchIcon, SearchInput, SearchWrapper, TaskInput } from "./style";
+
+const Input: React.FC<InputProps> = ({ kind, value, onChange, isDisabled }) => {
+  if (kind === InputTypes.TASK) {
+    return (
+      <TaskInput
+        type="text"
+        placeholder="Add new item..."
+        value={value}
+        onChange={onChange}
+        name="task"
+        disabled={isDisabled}
+      />
+    );
+  } else {
+    return (
+      <SearchWrapper>
+        <SearchInput
+          placeholder="Search items"
+          value={value}
+          onChange={onChange}
+          name="search"
+        />
+        <SearchIcon />
+      </SearchWrapper>
+    );
+  }
+};
+
+export default Input;
---
+++ src/types/input.ts
@@ -0,0 +1,17 @@
+import { ChangeEvent } from "react";
+
+export enum InputTypes {
+  SEARCH,
+  TASK
+}
+
+export interface InputProps {
+  kind: InputTypes,
+  value: string,
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+  isDisabled?: boolean 
+}
+
+export interface InputValues {
+  [key: string]: string;
+}
