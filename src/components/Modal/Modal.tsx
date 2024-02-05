26bce2f981e4796eb3fd23b05986c8abd1c297a5
luc0liv <luoliveiradacunha@gmail.com>
2024-02-05 01:18:56 -0300

    feat: add filters

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,15 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { ModalStyle } from "./style";
 import Input from "../Input/Input";
 import { InputTypes, InputValues } from "../../types/input";
 import Button from "../Button/Button";
-import { AddTaskWrapper, addTheme } from "../Button/style";
+import {
+  AddTaskWrapper,
+  addTheme,
+  doneSelectTheme,
+  doneTheme,
+} from "../Button/style";
 
 import { BsPlusCircleFill } from "react-icons/bs";
 import { TaskItem } from "../../types/task";
@@ -12,6 +18,8 @@ import {
   getFromLocalStorage,
   saveToLocalStorage,
 } from "../../helpers/localStorage";
+import Filters from "../Filters/Filters";
+import { ButtonProps } from "../../types/button";
 
 export default function Modal() {
   const [inputValues, setInputValues] = useState<InputValues>({
@@ -23,6 +31,7 @@ export default function Modal() {
   const [taskListFiltered, setTaskListFiltered] = useState<TaskItem[]>([]);
   const [lastId, setLastId] = useState<number>(0);
   const [isFiltered, setIsFiltered] = useState<boolean>(false);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const storedTasks = getFromLocalStorage("taskList");
@@ -50,17 +59,16 @@ export default function Modal() {
     );
   };
 
-
   const saveTask = () => {
     const storageTaskList = getFromLocalStorage("taskList");
     if (inputValues.task === "") {
       return;
     }
 
-    const item = { 
+    const item = {
       id: lastId + 1,
       task: inputValues.task,
-      isDone: false 
+      isDone: false,
     };
 
     if (storageTaskList) {
@@ -75,12 +83,16 @@ export default function Modal() {
       setLastId(item.id);
       saveToLocalStorage("taskList", newList);
     }
-    setInputValues({ task: "" });
+    setInputValues((prevState) => ({ ...prevState, task: "" }));
   };
 
   const onDelete = (index: number) => {
     const newTaskList = [...taskList];
     newTaskList.splice(index, 1);
+    // if (isFiltered) {
+    //   const filteredList = newTaskList.filter((task) => task.isDone === true);
+    //   setTaskListFiltered(filteredList);
+    // }
     setTaskList(newTaskList);
     saveToLocalStorage("taskList", newTaskList);
   };
@@ -90,24 +102,58 @@ export default function Modal() {
       task.id === index ? { ...task, isDone: true } : task
     );
 
+    // if (isFiltered) {
+    //   const filteredList = updatedTaskList.filter((task) => task.isDone === true);
+    //   setTaskListFiltered(filteredList);
+    // }
+
     saveToLocalStorage("taskList", updatedTaskList);
     setTaskList(updatedTaskList);
   };
 
   const filterTasksByStatus = (status: boolean) => {
-    const filteredByStatus = taskList.filter((task) => task.isDone === status);
+    const taskListToFilter = getFromLocalStorage("taskList");
+    const listParsed = taskListToFilter && JSON.parse(taskListToFilter);
+    const filteredByStatus: TaskItem[] = listParsed.filter(
+      (task: { isDone: boolean }) => task.isDone === status
+    );
+
     setTaskListFiltered(filteredByStatus);
+    status ? setFilter("done") : setFilter("pending");
     isFiltered ? setIsFiltered(false) : setIsFiltered(true);
+  };
+
+  const buttonsF: ButtonProps[] = [
+    {
+      theme: filter === "done" && isFiltered ? doneSelectTheme : doneTheme,
+      onButtonClick: () => filterTasksByStatus(true),
+      children: "Done",
+    },
+    {
+      theme: filter === "pending" && isFiltered ? doneSelectTheme : doneTheme,
+      onButtonClick: () => filterTasksByStatus(false),
+      children: "Pending",
+    },
+  ];
+
+  const searchTask = (e: ChangeEvent<HTMLInputElement>, list: TaskItem[]) => {
+    const { name, value } = e.target;
+    setInputValues((prevState) => ({ ...prevState, [name]: value }));
+    const searchTerm = value.toLocaleLowerCase();
+    const filtered = list.filter((t) => t.task.toLocaleLowerCase().includes(searchTerm));
+    setTaskListFiltered(filtered);
+    value === "" ? setIsFiltered(false) : setIsFiltered(true);
   }
 
   return (
     <ModalStyle>
-      <button onClick={() => filterTasksByStatus(true)}>DONE</button>
-      <button onClick={() => filterTasksByStatus(false)}>PENDING</button>
-      <Input
-        kind={InputTypes.SEARCH}
-        value={inputValues.search}
-        onChange={handleChange}
+      <Filters
+        input={{
+          value: inputValues.search,
+          kind: InputTypes.SEARCH,
+          onChange: (e: ChangeEvent<HTMLInputElement>) => searchTask(e, isFiltered ? taskListFiltered : taskList),
+        }}
+        buttons={buttonsF}
       />
       <AddTaskWrapper>
         <Input
